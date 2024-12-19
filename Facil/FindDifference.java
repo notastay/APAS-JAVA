67cf7559fc6165679351da9a4c22c6662c25ceb0
notastay <notastay@gmail.com>
2024-12-19 09:40:23 +0100

    9:40

---
+++ Facil/FindDifference.java
@@ -0,0 +1,17 @@
+package Facil;
+/* dado 2 strings s y t que consisten solo de letras minusculas.
+ * 
+ * String t esta generado aleatoriamente mezclando las letras del string s 
+ * y añadiendo una letra mas en una posicion al azar.
+ * 
+ * Encuentra la letra que se ha añadido en t.
+ */
+public class FindDifference {
+    public static void main(String[] args) {
+        
+    }
+
+    public static String letraDistinta(String s){
+        
+    }
+}
